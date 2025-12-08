f7e753d46447a0c78c018c2523358d8f815ff1c9
Rem <2858275841@qq.com>
2025-12-08 11:17:55 +0800

    style(App): 为根元素添加黄色边框以增强视觉区分

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ function handleFontSize(size: number) {
 </script>
 
 <template>
-  <div class="flex h-full flex-col text-black">
+  <div class="flex h-full flex-col border border-[#fbe961] text-black">
     <div class="drag h-5 cursor-move bg-[#fbe961]" />
     <div class="no-drag flex-1 bg-[#fdf3a7] p-2 outline-none" contenteditable />
   </div>
