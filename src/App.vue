00646f1bf8997e86ad309a215e2a1fe6a63b4d2e
Rem <2858275841@qq.com>
2025-12-12 10:03:14 +0800

    fix: 出现异常滚动条问题

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@ function handleFontSize(e: KeyboardEvent, minSize: number, defaultSize: Ref<numb
 <template>
   <div class="flex h-full flex-col border border-[#fbe961] text-black">
     <div class="drag h-5 cursor-move bg-[#fbe961]" />
-    <div ref="editRef" class="no-drag flex-1 bg-[#fdf3a7] p-2 text-2xl outline-none" contenteditable spellcheck="false" />
+    <div ref="editRef" class="no-drag flex-1 overflow-y-hidden bg-[#fdf3a7] p-2 text-2xl outline-none" contenteditable spellcheck="false" />
   </div>
 </template>
 
